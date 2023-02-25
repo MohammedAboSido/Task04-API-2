5dcd6a35095da89a35c29038724951b6e909237a
Mohammed A. Abo Sido <abosido2@gmail.com>
2023-02-25 14:37:14 +0200

    API-2 With Giphy

--- main.js
+++ main.js
@@ -2,53 +2,43 @@ let boxes = document.querySelector(".boxes");
 let toDown = document.querySelector(".to-down");
 let main = document.querySelector("main");
 let box = ``;
+boxes.innerHTML = "Loading...";
+fetch(
+  "https://api.giphy.com/v1/gifs/trending?api_key=VTgOb97pV9Q9MtChVSKLsccXGBiBTqjr"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    data.data.forEach((item) => {
+      box += `
+          <div class="box">
+            <img src=${item.images["480w_still"].url} alt=""/>
+          </div> 
+            `;
+    });
+    boxes.innerHTML = box;
+  })
+  .catch((error) => (boxes.innerHTML = error));
+
+const callApi = async (url) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    data.data.forEach((item) => {
+      box += `
+            <div class="box">
+              <img src=${item.images["480w_still"].url} alt=""/>
+            </div> 
+    `;
+    });
+    boxes.innerHTML = box;
+  } catch (error) {
+    boxes.innerHTML = error;
+  }
+};
+callApi(
+  "https://api.giphy.com/v1/gifs/trending?api_key=VTgOb97pV9Q9MtChVSKLsccXGBiBTqjr"
+);
 
 toDown.addEventListener("click", () => {
   window.scrollTo(0, document.body.scrollHeight);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const callApi = async (method, url) => {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open(method, url, true);
-//   xhr.send();
-//   return new Promise((resolve, reject) => {
-//     xhr.onreadystatechange = () => {
-//       if (xhr.readyState === 4 && xhr.status === 200) {
-//         resolve(JSON.parse(xhr.response));
-//       } else if (xhr.readyState === 4 && xhr.status !== 200) {
-//         reject(Error("went wrong"));
-//       }
-//     };
-//   });
-// };
-// boxes.innerHTML = "Loading...";
-// callApi("GET", "https://jsonplaceholder.typicode.com/posts")
-//   .then((data) => {
-//     data.forEach((item) => {
-//       box += `
-//         <div class="box">
-//         <h4 class="box-title">${item.title}</h4>
-//         <p class="box-desc">${item.body}</p>
-//         <div class="box-spans">
-//           <span class="span-one">P1</span>
-//           <span class="span-two">Heath</span>
-//         </div>
-//       </div>
-//         `;
-//     });
-//     boxes.innerHTML = box;
-//   })
-//   .catch((error) => (boxes.innerHTML = error));
